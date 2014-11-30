d6575e918e665d7f516e2820406cc6031d828c7f
Shubhank Srivastava <shubhanksrivastava94@gmail.com>
2014-11-30 17:41:17 +0530

    configuring app

---
+++ public/js/routes.js
@@ -0,0 +1,7 @@
+'use strict';
+
+Codeyard.config(['$stateProvider',
+  function($stateProvider) {
+
+  }
+]);
--- server.js
+++ server.js
@@ -2,16 +2,69 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
 var http = require('http').Server(app);
-http.listen(8080);
-console.log('Real Time Editor for Codeyard listening on 8080');
+var config = require('./config.json');
+var fs = require('fs');
+var async = require('async');
+
+app.engine('html', require('ejs').renderFile);
+app.set('env', process.env.NODE_ENV);
+app.set('view engine', 'ejs');
+app.set('views',__dirname+'/public/views');
+
 
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
-  app.use(bodyParser.urlencoded({
+app.use(bodyParser.urlencoded({
     extended: true
-  }));
-app.set('view engine', 'html');
+}));
+
+var JSaggregator = function(){
+    var dist = Object.keys(config.core.js)[0];
+    var JS = '';
+    async.eachSeries(config.core.js[dist],function(item,iterate){
+    	fs.readFile('./'+item,function(err,data){
+            if (err) {console.log('Could not load JS files.')};
+            JS = JS+'\n'+data;
+            iterate();
+    	});
+    },function(err){
+    	if (err) {console.log(err)};
+    	fs.writeFile(dist,JS,function(err,result){
+            console.log('CSS LOADED');
+    	});
+    });
+};
+
+var CSSaggregator = function(){
+    var dist = Object.keys(config.core.css)[0];
+    var CSS = '';
+    async.eachSeries(config.core.css[dist],function(item,iterate){
+    	fs.readFile('./'+item,function(err,data){
+            if (err) {console.log('Could not load CSS files.')};
+            CSS = CSS+'\n'+data;
+            iterate();
+    	});
+    },function(err){
+    	if (err) {console.log(err)};
+    	fs.writeFile(dist,CSS,function(err,result){
+           console.log('JS LOADED');
+    	});
+    });
+};
+
+app.use(/.*\.js/,function(req,res){
+	res.sendFile(__dirname+'/bower_components/dist.min.js');
+});
+
+app.use(/.*\.css/,function(req,res){
+	res.sendFile(__dirname+'/bower_components/dist.min.css');
+});
 
 app.get('/',function(req,res){
-	res.send('hello');
+    res.render('view.html');
 });
+
+CSSaggregator();
+JSaggregator();
+http.listen(8080);
+console.log('*********Real Time Editor for Codeyard listening on 8080**********');
